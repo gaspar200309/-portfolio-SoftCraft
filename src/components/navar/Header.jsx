494432382e896377f2e59b784db8aca95de5cab0
link200309 <jonhh.0023@gmail.com>
2024-08-16 14:16:05 -0400

    fix: header

--- src/components/navar/Header.jsx
+++ src/components/navar/Header.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+//css
+import "./Header.css";
+
+//react
+import { Link } from "react-router-dom";
+import { LiaCubesSolid } from "react-icons/lia";
 
 function Header() {
-    return (
-        <header>
-            <div className="logo">
-                <i className="fa-brands fa-codepen"></i>
-                <h1>Nombre</h1>
-            </div>
-            <nav id="nav">
-                <Link to="/">Inicio</Link>
-                <Link to="/servicios">Servicios</Link>
-                <Link to="/habilidades">Habilidades</Link>
-                <Link to="/trabajos">Trabajos</Link>
-                <Link to="/contacto">Contacto</Link>
-            </nav>
-            <div className="redes">
-                <a href="#"><i className="fa-brands fa-twitter"></i></a>
-                <a href="#"><i className="fa-brands fa-youtube"></i></a>
-                <a href="#"><i className="fa-brands fa-facebook-f"></i></a>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="logo">
+        <LiaCubesSolid />
+        <h1>Nombre</h1>
+      </div>
+      <nav id="nav">
+        <Link to="/">Inicio</Link>
+        <Link to="/servicios">Servicios</Link>
+        <Link to="/habilidades">Habilidades</Link>
+        <Link to="/trabajos">Trabajos</Link>
+        <Link to="/contacto">Contacto</Link>
+      </nav>
+      <div className="redes">
+        <a href="#">
+          <i className="fa-brands fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i className="fa-brands fa-youtube"></i>
+        </a>
+        <a href="#">
+          <i className="fa-brands fa-facebook-f"></i>
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
