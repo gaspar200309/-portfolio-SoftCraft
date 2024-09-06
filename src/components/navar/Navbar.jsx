283a056ab4e553f0a1aa7a010565f1a9ed6c889e
link200309 <jonhh.0023@gmail.com>
2024-09-05 23:42:25 -0400

    feat: nav responsive

--- src/components/navar/Navbar.jsx
+++ src/components/navar/Navbar.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import ImagenesApp from '../../assets/ImagenesApp';
-import { Link, useLocation } from 'react-router-dom';
+import { useState } from "react";
+import ImagenesApp from "../../assets/ImagenesApp";
+import { Link, useLocation } from "react-router-dom";
 import { FaFacebookSquare, FaYoutube, FaTimes } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -24,12 +24,42 @@ function Navbar() {
         <h1>SoftCraft</h1>
       </div>
 
-      <nav id="nav" className={menuOpen ? 'nav open' : 'nav'}>
-        <a href="/" onClick={closeMenu} className={location.pathname === '/' ? 'active' : ''}>Inicio</a>
-        <a href="#servicios" onClick={closeMenu} className={location.hash === '#servicios' ? 'active' : ''}>Servicios</a>
-        <a href="#habilidades" onClick={closeMenu} className={location.hash === '#habilidades' ? 'active' : ''}>Acerca de</a>
-        <a href="#trabajos" onClick={closeMenu} className={location.hash === '#trabajos' ? 'active' : ''}>Trabajos</a>
-        <a href="#contacto" onClick={closeMenu} className={location.hash === '#contacto' ? 'active' : ''}>Contáctenos</a>
+      <nav id="nav" className={menuOpen ? "nav open" : "nav"}>
+        <a
+          href="/"
+          onClick={closeMenu}
+          className={location.pathname === "/" ? "active" : ""}
+        >
+          Inicio
+        </a>
+        <a
+          href="#servicios"
+          onClick={closeMenu}
+          className={location.hash === "#servicios" ? "active" : ""}
+        >
+          Servicios
+        </a>
+        <a
+          href="#habilidades"
+          onClick={closeMenu}
+          className={location.hash === "#habilidades" ? "active" : ""}
+        >
+          Acerca de
+        </a>
+        <a
+          href="#trabajos"
+          onClick={closeMenu}
+          className={location.hash === "#trabajos" ? "active" : ""}
+        >
+          Trabajos
+        </a>
+        <a
+          href="#contacto"
+          onClick={closeMenu}
+          className={location.hash === "#contacto" ? "active" : ""}
+        >
+          Contáctenos
+        </a>
       </nav>
 
       <div id="icono-nav" onClick={toggleMenu}>
@@ -37,8 +67,16 @@ function Navbar() {
       </div>
 
       <div className="redes">
-        <Link title="Visítanos en YouTube" to="/" onClick={closeMenu}><FaYoutube /></Link>
-        <Link title="No te pierdas nuestros post más recientes en Facebook"to="/" onClick={closeMenu}><FaFacebookSquare /></Link>
+        <Link title="Visítanos en YouTube" to="/" onClick={closeMenu}>
+          <FaYoutube />
+        </Link>
+        <Link
+          title="No te pierdas nuestros post más recientes en Facebook"
+          to="/"
+          onClick={closeMenu}
+        >
+          <FaFacebookSquare />
+        </Link>
       </div>
     </header>
   );
